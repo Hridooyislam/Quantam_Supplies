6a90861e5e4627172b9c3668b49b0a7b08472814
Hridooyislam <you@example.comislam111hridoy@gmail.com>
2023-08-22 22:20:31 +0600

    Some fixes

---
+++ cart.js
@@ -0,0 +1,106 @@
+let total_sell;
+total_sell=0;
+const cart_total_price=document.getElementById('total_price');
+const cart_list=document.getElementById('cart_list')
+// --------------------------------------------------------------------------
+document.getElementById('K-accesories-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('k-accesories-price').innerText);
+    const product_name=document.getElementById('k-accesories').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+    console.log(cart_list.innerText);
+})
+
+document.getElementById('k-accesories-2-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('k-accesories-2-price').innerText);
+    const product_name=document.getElementById('k-accesories-2').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+}) 
+
+
+document.getElementById('home-cooker-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('home-cooker-price').innerText);
+    const product_name=document.getElementById('home-cooker').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+}) 
+
+document.getElementById('sports-cap-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('sports-cap-price').innerText);
+    const product_name=document.getElementById('sports-cap').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+}) 
+
+document.getElementById('jursey-set-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('jursey-set-price').innerText);
+    const product_name=document.getElementById('jursey-set').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+}) 
+
+document.getElementById('sports-cates-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('sports-cates-price').innerText);
+    const product_name=document.getElementById('sports-cates').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+}) 
+
+
+document.getElementById('relax-chair-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('relax-chair-price').innerText);
+    const product_name=document.getElementById('relax-chair').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+})
+
+document.getElementById('children-play-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('children-play-price').innerText);
+    const product_name=document.getElementById('children-play').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+})
+
+document.getElementById('flexible-sofa-p-btn').addEventListener('click',function K_accesories(){
+    const product_price=parseFloat(document.getElementById('flexible-sofa-price').innerText);
+    const product_name=document.getElementById('flexible-sofa').innerText;
+    const li=document.createElement('li');
+    li.innerText=product_name;
+    cart_list.appendChild(li);
+    total_sell+=product_price;
+    cart_total_price.innerText=total_sell;
+})
+// --------------------------------------------------------
+
+
+console.log(cart_total_price.innerText);
+if (cart_total_price.innerText>0) {
+    console.log('ok');
+    document.getElementById('purchase-btn').removeAttribute('disabled');
+}
+
